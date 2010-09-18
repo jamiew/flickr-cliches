82d38d3de95ea0f9fb69c4c595058c5169657948
Jamie Wilkinson <jamie@tramchase.com>
2010-09-18 13:45:13 -0700

    Don't try to double-auth if we have cache token already

--- interesting_by_geo.rb
+++ interesting_by_geo.rb
@@ -8,15 +8,17 @@ config_file = File.dirname(__FILE__)+'/config.yml'
 config = YAML.load(File.open(config_file))
 puts "config=#{config.inspect}"
 
-puts "Authorizing Flickr..."
-puts "This will launch your web browser (on a Mac)."
-puts "Press 'enter' after authorizing to continue."
 flickr = Flickr.new(config_file)
-puts flickr.inspect
-auth_url = flickr.auth.url(:read)
-`open '#{auth_url}'` # Fire up your browser
-gets
-flickr.auth.cache_token
+if File.exists?(File.dirname(__FILE__)+'/'+flickr.token_cache)
+  puts "Already authorized! Delete #{flickr.token_cache} to reset"
+else
+  puts "Authorizing Flickr... we'll now launch your web browser (on a Mac)"
+  puts "Click 'Authorize Yo', then come back here & press enter to continue."
+  auth_url = flickr.auth.url(:read)
+  `open '#{auth_url}'`
+  gets
+  flickr.auth.cache_token
+end
 
 opts = {:media => 'photo', :sort => 'interestingness-desc'}
 opts[:place_id] = 'fh4BQ2yYA5lK_1zfsw' # semi-random
@@ -25,9 +27,9 @@ opts[:place_id] = 'fh4BQ2yYA5lK_1zfsw' # semi-random
 puts "Searching for interesting photos..."
 photos = flickr.photos.search(opts)
 
+puts "1st photo = "
+puts photos[0].inspect
 
 puts "TODO doing something with these photos"
 
 exit 0
-
-
