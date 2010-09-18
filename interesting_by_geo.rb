752c371bda08a45da6e74decbbf1f51580cd191c
Jamie Wilkinson <jamie@tramchase.com>
2010-09-18 14:14:15 -0700

    Use bounding boxes correctly, and open all the links if you roll OPEN=1

--- interesting_by_geo.rb
+++ interesting_by_geo.rb
@@ -20,16 +20,18 @@ else
   flickr.auth.cache_token
 end
 
-opts = {:media => 'photo', :sort => 'interestingness-desc'}
-opts[:place_id] = 'fh4BQ2yYA5lK_1zfsw' # semi-random
-# opts[:bbox] = config[:bounding_box] # TODO
+opts = {:per_page => 10, :media => 'photo', :sort => 'interestingness-desc'}
+# opts[:place_id] = 'fh4BQ2yYA5lK_1zfsw' # semi-random
+box = config["bounding_box"]
+# bbox arg = min long, min lat, max long, max lat
+opts[:bbox] = "#{box[0][1]},#{box[0][0]},#{box[1][1]},#{box[1][0]}"
+puts opts[:bbox].inspect
 
 puts "Searching for interesting photos..."
 photos = flickr.photos.search(opts)
-
-puts "1st photo = "
-puts photos[0].inspect
-
-puts "TODO doing something with these photos"
+photos.each do |photo|
+  puts "Views: #{photo.views} \tLocation: #{photo.location.latitude},#{photo.location.longitude} \tURL: #{photo.photopage_url}"
+  `open '#{photo.photopage_url}'` if ENV['OPEN']
+end
 
 exit 0
