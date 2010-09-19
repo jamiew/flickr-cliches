9167f1601ad96006b57ed36dd986a5c7b91fbb28
Jamie Wilkinson <jamie@tramchase.com>
2010-09-18 18:11:35 -0700

    rm datasets and old lab file

--- lab/tag_parsing.rb
+++
@@ -1,5 +0,0 @@
-tags = '"wow its a phrase" blahhahaha wow "another phrase" jeez'
-phrases = tags.scan(/(\".*?\")/).flatten
-singles = tags.gsub(Regexp.new(phrases.join("|")), "").gsub(/ +/, " ").split(" ")
-phrases = phrases
-sanitized_tags = (singles+phrases).join(",")
