c805f4d4763fa93d61a7515fd599d4176a2ba7ed
Jamie Wilkinson <jamie@tramchase.com>
2010-09-18 19:13:34 -0700

    Create one combined output CSV of all data, with a 'country' column

--- interesting_by_geo.rb
+++ interesting_by_geo.rb
@@ -18,13 +18,17 @@ end
 page = ENV['PAGE'] && ENV['PAGE'].to_i || 1
 per_page = ENV['PER_PAGE'] && ENV['PER_PAGE'].to_i || 10
 sort = 'interestingness-desc'
+
+data_dir = File.dirname(__FILE__)+"/data"
+data_files = []
 bounding_boxes = YAML.load(File.open(File.dirname(__FILE__)+'/bounding_boxes.yml'))
+
 bounding_boxes.each do |hash|
 
   country = hash.keys[0]
   box = hash.values[0]
   safe_country = country.downcase.gsub(' ','_').gsub(',','')
-  country_dir = File.dirname(__FILE__)+"/data/#{safe_country}"
+  country_dir = "#{data_dir}/#{safe_country}"
   FileUtils.mkdir_p(country_dir)
 
   bbox = "#{box[0][1]},#{box[0][0]},#{box[1][1]},#{box[1][0]}"
@@ -39,7 +43,7 @@ bounding_boxes.each do |hash|
     photo_id = photo.url.split('/')[-1]
     location_info = photo.location && "#{photo.location && photo.location.latitude},#{photo.location && photo.location.longitude}] (#{photo.location.accuracy})" || 'nil'
     puts "Views: #{photo.views}  Location: #{location_info} \tDate: #{photo.taken_at.to_s}  URL: #{photo.photopage_url}"
-    data << {"photo_id" => photo_id, "photopage_url" => photo.photopage_url, "owner" => photo.owner, "owner_realname" => photo.owner_realname, "taken_at" => photo.taken_at.strftime("%Y-%m-%d %H:%M:%S"), "updated_at" => photo.updated_at.strftime("%Y-%m-%d %H:%M:%S"), "lat" => photo.location.latitude, "lon" => photo.location.longitude, "views" => photo.views, "tags" => sanitize_tags(photo.tags)}
+    data << {"country" => safe_country, "photo_id" => photo_id, "photopage_url" => photo.photopage_url, "owner" => photo.owner, "owner_realname" => photo.owner_realname, "taken_at" => photo.taken_at.strftime("%Y-%m-%d %H:%M:%S"), "updated_at" => photo.updated_at.strftime("%Y-%m-%d %H:%M:%S"), "lat" => photo.location.latitude, "lon" => photo.location.longitude, "views" => photo.views, "tags" => sanitize_tags(photo.tags)}
 
     # Data we want to dump:
     # - photo_pageurl, owner, owner_realname, taken_at, updated_at, latitude, longitude, views, tags
@@ -60,7 +64,8 @@ bounding_boxes.each do |hash|
   end
 
   STDERR.puts "Dumping raw CSV data..."
-  FasterCSV.open("#{country_dir}/data.csv", "w") do |csv|
+  data_file = "#{country_dir}/data.csv"
+  FasterCSV.open(data_file, "w") do |csv|
     headers = data.first.keys
     csv << headers
     data.each do |datum|
@@ -68,6 +73,21 @@ bounding_boxes.each do |hash|
     end
   end
 
+  data_files << data_file
+end
+
+# Combine all CSVs into one mega CSV
+puts "\nCombining into one output CSV..."
+puts data_files.inspect
+output = "#{data_dir}/combined.csv"
+header_done = false
+data_files.each do |file|
+  if header_done
+    `sed '1d' '#{file}' >> '#{output}'`
+  else
+    `cat '#{file}' > '#{output}'`
+    header_done = true
+  end
 end
 
 puts "Done."
